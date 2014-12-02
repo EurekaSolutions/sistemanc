49de9ec22fe6562e3d9263ed79bdf9fc921fd047
Edgar Leal <edgar.leal0@gmail.com>
2014-12-02 21:37:14 +0000

    Recuperacion de contraseña.

--- compras/protected/modules/usr/views/emails/verify.php
+++ compras/protected/modules/usr/views/emails/verify.php
@@ -1,13 +1,22 @@
 <p>
-	This message contains instructions to verify this email address. It was requested on the <?php echo CHtml::link(Yii::app()->name, $siteUrl); ?> website.
-    If you did not perform this request, please ignore this email or contact our administrator.
+	This message contains instructions to verify this email address. La solicitud fue realizada a través de la página web <?php echo CHtml::link(Yii::app()->name, $siteUrl); ?>.
+    Si usted no realizo esta solicitud, por favor ignore este correo o pongase en contacto (xxxxx@snc.com) con el administrador del sitio.
 </p>
 
-<p>To verify this email address, open the following link:</p>
+<p>haz clic aquí para cambiar tu contraseña:</p>
+
 <p>
 <?php echo CHtml::link($actionUrl, $actionUrl); ?>
 </p>
+
 <p>
-If the link does not open correctly, try copying it and pasting in the browser's address bar.
+Si el link no abre correctamente, copie y pegue en la barra de direcciones de su navegador preferido.
 </p>
 
+<p>
+	<h4>Recomendaciones:</h4>
+	<ul>
+		<li>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</li>
+		<li>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</li>
+	</ul>
+</p>
