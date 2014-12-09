9b2f5e5924fc36b58e22ab210376be9f4035c8e6
Edgar Leal <edgar.leal0@gmail.com>
2014-12-09 10:23:55 +0000

    vista parcial.

--- compras/protected/views/planificacion/vistaparcial.php
+++ compras/protected/views/planificacion/vistaparcial.php
@@ -2,9 +2,12 @@
 /* @var $this PlanificacionController */
 
 $this->breadcrumbs=array(
-	'Planificacion',
+	'Planificacion'=>array('/planificacion'),
+	'Vistaparcial',
 );
+
 ?>
+
 <style>
 tr.principaltr td {
     /*border: 1px solid black;*/
@@ -18,35 +21,12 @@ tr.principaltr th {
     vertical-align:middle;
 }
 </style>
-<?php $mfinal = 0;
-
- if(isset($usuario->enteOrgano->enteAdscrito))
- 	$padre = $usuario->enteOrgano->enteAdscrito->padre;
-?>
+<?php $mfinal = 0; ?>
 <!--<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>-->
-		<table data-toggle="table" data-url="data1.json" data-cache="false" data-height="">
-		    <thead>
-		        <tr class="principaltr">
-		            <th data-field="conapre">Mi código Onapre</th>
-		            <th data-field="nombreoue">Nombre Organo u Ente</th>
-		            <th data-field="tipo">Tipo</th>
-		            <th data-field="oadscripcion">Órgano de adscripción</th>
-		            <th data-field="conaprepadre">Código Onapre</th>
-		        </tr>
-		    </thead>
-		    <tbody>
-		    	<tr class="principaltr">
-
-		    		<td><?php echo $usuario->enteOrgano->codigo_onapre;?></td>
-		    		<td><?php echo $usuario->enteOrgano->nombre;?></td>
-		    		<td><?php echo $usuario->enteOrgano->tipo == 'E' ? 'ENTE' : 'ORGANO';?></td>
-		    		<td><?php echo isset($padre)? $padre->nombre:'NO APLICA';?></td>
-		    		<td><?php echo isset($padre)? $padre->codigo_onapre:'NO APLICA'; ?></td>
-		    	</tr>
-		    </tbody>
-		</table>
-	
 		
+		<h3 style="text-align: center;">ESTADO DE CARGA</h3><br>
+
+
 		<h4 style="text-align: center;">ACCIONES CENTRALIZADAS</h4><br>
 
 		<table data-toggle="table" data-url="data1.json" data-cache="false" data-height="">
@@ -54,18 +34,22 @@ tr.principaltr th {
 		        <tr class="principaltr">
 		            <th data-field="conapre">Código</th>
 		            <th data-field="nombreoue">Denominación</th>
-		            <th data-field="tipo">Bs.</th>
+		            <th data-field="tipo">Bs. Solicitados</th>
+		            <th data-field="tipo">Bs. Comprometidos</th>
+		            <th data-field="tipo">Bs.Solicitados - Bs.Comprometidos</th>
 		        </tr>
 		    </thead>
 		    <tbody>
-			 
 			 <?php   foreach ($acciones as $key => $value) { 
-			 				$monto = $this->montoAccion($value);?>
+			 				$monto = $this->montoAccion($value);
+			 ?>
 		
 		    	<tr class="principaltr">
 		    		<td><?php echo $value->codigo_accion; ?></td>
 		    		<td><?php echo $value->accion->nombre; ?></td>
 		    		<td><?php echo number_format($monto,2,',','.'); ?></td>
+		    		<td><?php echo number_format($valor = rand(0,$monto),2,',','.'); ?></td>
+		    		<td><strong><?php echo number_format($monto - $valor,2,',','.');?></strong></td>
 		    	</tr>
 
 		    <?php $mfinal += $monto;
@@ -81,23 +65,23 @@ tr.principaltr th {
 		        <tr class="principaltr">
 		            <th data-field="conapre">Código</th>
 		            <th data-field="nombreoue">Denominación</th>
-		            <th data-field="tipo">Bs.</th>
+		            <th data-field="tipo">Bs. Solicitados</th>
+		            <th data-field="tipo">Bs. Comprometidos</th>
+		            <th data-field="tipo">Bs.Solicitados - Bs.Comprometidos</th>
 		        </tr>
 		    </thead>
 		    <tbody>
 
-			<?php  foreach ($proyectos as $key => $value){
-						$monto = $this->montoProyecto($value); 
-			?> 
+			<?php  foreach ($proyectos as $key => $value){ 
+						$monto = $this->montoProyecto($value);?> 
 		    	<tr class="principaltr">
 		    		<td><?php echo $value->codigo; ?></td>
 		    		<td><?php echo $value->nombre; ?></td>
 		    		<td><?php echo number_format($monto,2,',','.'); ?></td>
+		    		<td><?php echo number_format($valor = rand(0,$monto),2,',','.'); ?></td>
+		    		<td><strong><?php echo number_format($monto - $valor,2,',','.');?></strong></td>
 		    	</tr>
 		     <?php $mfinal += $monto;
 				} ?>
 		    </tbody>
-		</table>	
-
-		<BR><BR>
-				<p class="text-right"><STRONG>TOTAL COMPRAS</STRONG>: <?php echo number_format($mfinal,2,',','.'); ?> <STRONG>(Bs. F)</STRONG></p>
+		</table>
