415116f96f45fb5d2e34a6240c675ad028770265
Marcos Ramírez <marcospha@gmail.com>
2015-03-16 19:08:45 +0000

    Eliminadas carpetas respaldo.

--- compras/protected/data/views/layouts/planificacion.php
+++
@@ -1,8 +0,0 @@
-<?php /* @var $this Controller */ ?>
-<?php $this->beginContent('//layouts/main'); ?>
-		<h3 style="text-align: center;">PLAN DE COMPRAS DEL ESTADO <span class="label label-default"><?php echo intval(date("Y")+1);?></span></h3>
-		<br>
-	    <div class="row show-grid">
-			<?php echo $content; ?>
-		</div>
-<?php $this->endContent(); ?>
--- compras/protected/data/views/planificacion/importacion.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* @var $this PlanificacionController */
-
-$this->breadcrumbs=array(
-	'Planificacion'=>array('/planificacion'),
-	'Importacion',
-);
-?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
